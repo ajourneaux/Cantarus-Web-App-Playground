5ab0b803415cc999eb3c995d08aa9d7247cd2b10
ajourneaux <ajourneaux@hotmail.co.uk>
2026-02-12 19:23:06 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Cantarus-Web-App-Playground/',
   plugins: [react()],
